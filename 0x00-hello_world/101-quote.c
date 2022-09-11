b2eb0fd483f528598ee7132870669c30a919bdde
Jennyjills <adiejennifer77@gmail.com>
2022-09-11 05:55:49 -0800

    task 8

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+* main - prints exactly "and that piece of art is useful"
+* followed by a new line to the standrd error
+* Return: 0 if exited sucessful. otherwise 1
+*/
+int main(void)
+{
+write("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+return (0);
+}
