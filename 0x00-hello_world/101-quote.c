6d49b9a463ef11f01b8e71aded2e7f9b51f3c29d
Jennyjills <adiejennifer77@gmail.com>
2022-09-11 06:40:35 -0800

    task 8

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,13 @@
 #include <stdio.h>
+#include <unistd.h>
 /**
 * main - prints exactly "and that piece of art is useful"
-* followed by a new line to the standrd error
-* Return: 0 if exited sucessful. otherwise 1
+* - Dora Korpar, 2015-10-19",
+* followed by a new line, to the standrd error.
+* Return: Always 0 (sucess)
 */
 int main(void)
 {
-write(2, and that piece of art is useful\" - Dora Korpar, 2015-10-19\n, 59);
+write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 return (1);
 }
