858bc6348f7704535a6e2e5dc06a69fc62e83da8
Jennyjills <adiejennifer77@gmail.com>
2022-09-11 00:35:46 -0800

    task 5

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -3,8 +3,8 @@
 * main - prints the phrase with proper grammar, but the outcome is a piece of art,
 * Return: 0 if exited properly or non zero if otherwise
 */
-{
 int main (void)
+{
 printf(with proper grammar, but the outcome is a piece of art,)
 Return (0)
 }
