14e9562e84964661d7afb1ab12bb1105630a4d21
Jennyjills <adiejennifer77@gmail.com>
2022-09-12 07:40:14 -0800

    task5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main -> prints exactly the alphabets in lowercase
+ * Return : always 0
+ */
+
+int main(void)
+{
+	int n;
+
+	for (n = 0; n<10;n++)
+	{
+		printf("%d\n", n);
+		putchar(ch);
+	}
+
+return (0);
+}
