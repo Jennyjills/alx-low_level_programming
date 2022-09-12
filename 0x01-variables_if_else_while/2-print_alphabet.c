f4e387cdb5a0713aac393166f23db8bae5ccf87e
Jennyjills <adiejennifer77@gmail.com>
2022-09-12 07:01:20 -0800

    task 2

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -4,13 +4,15 @@
  * main -> prints exactly the alphabets in lowercase
  * Return : always 0
  */
+
 int main(void)
 {
 	int ch;
+	
 	for (ch = 97; ch <= 122; ch++)
 	{
 		putchar(ch);
-	 }
+	}
 	putchar(10); /** this is ascii code for new line) */
 
 	return (0);
