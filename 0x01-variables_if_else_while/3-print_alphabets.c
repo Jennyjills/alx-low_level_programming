8774fa86556a11b686b4a923002a6ed02b8cb3fc
Jennyjills <adiejennifer77@gmail.com>
2022-09-12 07:17:39 -0800

    task 3

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main -> prints exactly the alphabets in lowercase
+ * Return : always 0
+ */
+
+int main(void)
+{
+	int ch;
+
+	c
+	putchar(10); /** this is ascii code for new line) */
+
+	return (0);
+}
