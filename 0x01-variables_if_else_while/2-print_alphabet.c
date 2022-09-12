c8514d724cae06f173f37de9ef27d646db3f9ac9
Jennyjills <adiejennifer77@gmail.com>
2022-09-12 06:57:37 -0800

    task 2

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -6,12 +6,12 @@
  */
 int main(void)
 {
-    int ch;
-    for (ch = 97; ch <= 122; ch++)
-    {
-        putchar(ch);
-     }
-    putchar(10); /** this is ascii code for new line) */
+	int ch;
+	for (ch = 97; ch <= 122; ch++)
+	{
+		putchar(ch);
+	 }
+	putchar(10); /** this is ascii code for new line) */
 
-    return (0);
+	return (0);
 }
