d4ff9bada5d42239b25eda5d27e9ee32ebc5bbdb
Jennyjills <adiejennifer77@gmail.com>
2022-09-13 17:38:34 -0800

    task 0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -3,13 +3,20 @@
 /**
  * main -> prints exactly _putchar
  *
- * Return : always 0
+ * Return : always 0 on sucess
  */
 
 int main(void)
 {
-	char ch = '\_putchar'
-	putchar(ch);
+	char 's' = "\_putchar";
+	int i = 0;
 
+	while(s(i) != 0)
+	{
+	_putchar(s(i));
+	i++
+	}
+	_putchar(10)
+	
 	return (0);
 }
